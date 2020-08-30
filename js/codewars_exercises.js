340f6679a9aee65f1f68cf3707e4bdc1ec3f08ae
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-08-29 22:50:18 -0500

    feat: get the mean of an array exercise complete.

--- js/codewars_exercises.js
+++ js/codewars_exercises.js
@@ -12,3 +12,20 @@ console.log("Number of People in the Bus");
 console.log(peopleOnBus([[10,0],[3,5],[5,8]]));
 console.log(peopleOnBus([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]));
 console.log(peopleOnBus([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]));
+
+
+/* Get the Mean of an Array */
+
+function getAverage(marks){
+    var total = 0
+    for (var i = 0; i < marks.length; i++) {
+        total += marks[i]
+
+    }
+    return Math.floor(total / marks.length);
+}
+
+console.log("Get the Mean of an Array");
+console.log(getAverage([2,2,2,2]));
+console.log(getAverage([1,3,5,7,9,11,13,15,17,19]));
+console.log(getAverage([1,1,1,1,1,1,1,2]));
