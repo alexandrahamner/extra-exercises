5add41aa125dc382985d2df3997cdc0948ce364c
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-03 21:43:36 -0600

    feat: completed a codewars exercise, evenOrOdd.

--- js/codewars_exercises.js
+++ js/codewars_exercises.js
@@ -130,3 +130,35 @@ function count (string) {
     });
     return count;
 }
+
+
+/*
+Given a string of numbers confirm whether the total of all the individual even numbers are greater than the total of all the indiviudal odd numbers. Always a string of numbers will be given.
+
+If the sum of even numbers is greater than the odd numbers return:
+
+'Even is greater than Odd'
+
+If the sum of odd numbers is greater than the sum of even numbers return:
+
+'Odd is greater than Even'
+
+If the total of both even and odd numbers are identical return:
+
+'Even and Odd are the same'
+
+
+ */
+
+function evenOrOdd(str) {
+    var even = 0, odd = 0;
+    str.split("").forEach(function(num){
+        if (num % 2 === 0) {
+            even += +num;
+        } else {
+            odd += +num;
+        }
+    });
+    if (even === odd) return "Even and Odd are the same";
+    return even > odd  ? "Even is greater than Odd" : "Odd is greater than Even"
+}
