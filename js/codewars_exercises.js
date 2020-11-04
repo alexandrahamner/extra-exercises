2a8e62280c9c75d7cd6a504fa939f08e5057f875
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-03 22:22:14 -0600

    feat: completed codewars exercise, oddOrEven.

--- js/codewars_exercises.js
+++ js/codewars_exercises.js
@@ -170,3 +170,20 @@ Given a set of numbers, return the additive inverse of each. Each positive becom
 function invert(array) {
     return array.map( x => x === 0 ? x : -x);
 }
+
+/*
+Given a list of numbers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+ */
+
+function oddOrEven(array) {
+
+    const sum = array.reduce((accumulation, currentNumber) => {
+        return accumulation + currentNumber;
+    }, 0);
+
+    return (sum % 2 === 0) ? "even" : "odd";
+}
