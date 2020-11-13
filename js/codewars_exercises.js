90fef714947e1752d44bae406e74853e2934e0ac
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-12 20:12:38 -0600

    feat: completed isPalindrome function from leetcode.

--- js/codewars_exercises.js
+++ js/codewars_exercises.js
@@ -213,3 +213,28 @@ var twoSum = function(nums, target) {
         numberIndex.set(num, i);
     }
 };
+
+/*
+
+ */
+
+var isPalindrome = function(x) {
+    // Special cases:
+    // As discussed above, when x < 0, x is not a palindrome.
+    // Also if the last digit of the number is 0, in order to be a palindrome,
+    // the first digit of the number also needs to be 0.
+    // Only 0 satisfy this property.
+   if(x < 0 || x % 10 === 0 && x !== 0) {
+       return false;
+   }
+    return x === reversedDigits(x);
+};
+
+var reversedDigits = function(x) {
+    let reversed = 0;
+    while(x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return reversed;
+}
